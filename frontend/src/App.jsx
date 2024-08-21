96cc208a82788bde1a69d261c6cc693c5a61da86
shoeb310 <sshoeb532@gmail.com>
2024-08-21 16:59:31 +0530

    project-init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+    <div className="red">
+      Hello Madarchod
+      </div>
+      <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+    </>
+  )
+}
+
+export default App
